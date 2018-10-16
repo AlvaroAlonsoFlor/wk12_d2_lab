93481dd9cbce9c4a119ce842de780e448b7484e9
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-10-16 12:15:20 +0100

    Add component abstract class

--- src/main/java/Component.java
+++ src/main/java/Component.java
@@ -1,2 +1,19 @@
-public class Component {
+public abstract class Component {
+
+    private String model;
+    private String make;
+
+    public Component(String model, String make) {
+        this.model = model;
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getMake() {
+        return make;
+    }
 }
+
